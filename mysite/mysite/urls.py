d729bdd4b7ebdc27c3944b4dab98451cf70a461d
Schuyler Schwafel <saschwafel@gmail.com>
2015-02-24 20:46:14 -0800

    Finished in-class work

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include('myblog.urls')),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {'template_name': 'login.html'},
+        name="login"),
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        {'next_page': '/'},
+        name="logout"),
+    url(r'^admin/', include(admin.site.urls)), 
+)
